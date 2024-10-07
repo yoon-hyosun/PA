cc8ef38fdb976d99a327f3862d23bf7d9d3a461a
yoon-hyosun <127287174+yoon-hyosun@users.noreply.github.com>
2024-10-08 08:22:05 +0900

    Add files via upload

---
+++ first.py
@@ -0,0 +1,26 @@
+first='''
+<html>
+    <head>
+      <title> HTML연습 </title>
+    </head>
+    <body>
+        <p  align="center"> 홍길동 </p>
+        <p  align="left"> 이순신 </p>
+        <p  align="right"> 유관순 </p>
+    </body>
+</html>
+'''
+
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(first, 'html.parser')
+
+print(soup.find('p'))
+print(soup.find('p', align='right'))
+print(soup.find_all('p'))
+print()
+
+
+result = soup.find_all('p')
+
+for i in result:
+    print(i.get_text())
