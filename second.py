7c60f90f8fb0f586cd1215fe0d14275829e311f0
yoon-hyosun <127287174+yoon-hyosun@users.noreply.github.com>
2024-10-15 12:04:33 +0900

    Add files via upload

--- second.py
+++ second.py
@@ -5,6 +5,7 @@ with open(filename,'r', encoding='utf-8') as ex:
 
 print(soup.find('p','name2'))
 print()
+
 print(soup.find('p',id='fruits2'))
 print()
 
@@ -23,20 +24,16 @@ for i in result :
     f.write(txt+'\n')
 f.close()
 
-print('='*10)
+print('============================='*10)
+
 
 print(soup.select('p'))
-print('='*10)
+print('---------------'*10)
 print(soup.select('p.name2'))
 print(soup.select('.name2'))
 print(soup.select('#fruits2'))
-print(soup.select('p.name2 > span'))
-print(soup.select('p.name2 > span.store'))
-print(soup.select('.name2 > span.store'))
-print(soup.select('#fruits2 > span.store'))
 
-print(soup.select('a[href]'))
-print(soup.select('a')[0])
+
 
 
 
